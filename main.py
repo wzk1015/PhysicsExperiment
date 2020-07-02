3fc4222a836c791eba2621541621000f2ecdf283
WingDr <krq2000@126.com>
2020-07-02 18:05:18 +0800

    修改main.py，分开基物实验1和2

--- main.py
+++ main.py
@@ -7,19 +7,27 @@ import Frank_Hertz.frank_hertz as fh
 if __name__ == '__main__':
 	# 主程序，只引用模块
 	try:
-		print("目前可计算的实验：")
-		print("\t1、密里根油滴实验")
-		print("\t2、太阳能电池特性实验")
-		print("\t3、弗兰克赫兹实验")
-		exp = input("请输入实验序号：")
-		if exp == "1":
-			Millikan.Millikan()
-		elif exp == "2":
-			sb.SolarBattery()
-		elif exp == "3":
-			fh.FrankHertz()
-		else:
-			print("很抱歉。暂时没有相应的数据处理程序。")
+		term = input("请选择实验所属：\n1：基物实验1\n2：基物实验2\n（请输入序号）：")
+		if term == "1":
+			print("目前基物实验1中可计算的实验：")
+			print("\t1、迈克尔逊干涉实验（1091）")
+			exp = input("请输入实验序号：")
+			if exp == "1":
+				Millikan.Millikan()
+		elif term == "2":
+			print("目前基物实验2中可计算的实验：")
+			print("\t1、密里根油滴实验")
+			print("\t2、太阳能电池特性实验")
+			print("\t3、弗兰克赫兹实验")
+			exp = input("请输入实验序号：")
+			if exp == "1":
+				Millikan.Millikan()
+			elif exp == "2":
+				sb.SolarBattery()
+			elif exp == "3":
+				fh.FrankHertz()
+			else:
+				print("很抱歉。暂时没有相应的数据处理程序。")
 	except ValueError:
 		print("请输入一个数字")
 	input("点击任意键退出")
