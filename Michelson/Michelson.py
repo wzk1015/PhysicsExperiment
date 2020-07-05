55a63c9a9442261220494d361e3a6c7073a93fa2
KICKOFFMYBUTT <dhy_120@163.com>
2020-07-05 12:12:29 +0800

    添加了选择实验预习或数据处理的选项

--- Michelson/Michelson.py
+++ Michelson/Michelson.py
@@ -5,8 +5,7 @@ import os
 from numpy import sqrt, abs
 
 import sys
-sys.path.append('..')
-#print(sys.path)
+sys.path.append('..') # 如果最终要从main.py调用，则删掉这句
 from GeneralMethod.PyCalcLib import Method
 from reportwriter.ReportWriter import ReportWriter
 
@@ -21,24 +20,47 @@ class Michelson:
         "u_N", "u_lbd_lbd", "u_lbd",  # 不确定度的合成
         "final" # 最终结果
     ]
-
+    PREVIEW_FILENAME = "Preview.pdf"
+    DATA_SHEET_FILENAME = "data.xlsx"
+    REPORT_TEMPLATE_FILENAME = "Michelson_empty.docx"
+    REPORT_OUTPUT_FILENAME = "Michelson_out.docx"
     def __init__(self):
         self.data = {} # 存放实验中的各个物理量
         self.uncertainty = {} # 存放物理量的不确定度
-        self.report_data = {}
-
-        print("即将打开数据输入文件......")
-        # os.startfile("data.xlsx")
-        input("输入数据完成后请保存并关闭excel文件，然后按回车键继续")
-        self.input_data("./data.xlsx") # './' is necessary when running this file, but should be removed if run main.py
-        print("数据读入完毕，处理中......")
-        self.calc_data()
-        self.calc_uncertainty()
-        # print(self.data)
-        print("正在生成实验报告......")
-        self.fill_report()
-        print("Done!")
-
+        self.report_data = {} # 存放需要填入实验报告的
+        print("1091 迈克尔逊干涉仪实验\n1. 实验预习\n2. 数据处理")
+        while True:
+            try:
+                oper = input("请选择: ").strip()
+            except EOFError:
+                sys.exit(0)
+            if oper != '1' and oper != '2':
+                print("输入内容非法！请输入一个数字1或2")
+            else:
+                break
+        if oper == '1':
+            print("现在开始实验预习")
+            print("正在打开预习报告......")
+            os.startfile(self.PREVIEW_FILENAME)
+        elif oper == '2':
+            print("现在开始数据处理")
+            print("即将打开数据输入文件......")
+            # 打开数据输入文件
+            os.startfile(self.DATA_SHEET_FILENAME)
+            input("输入数据完成后请保存并关闭excel文件，然后按回车键继续")
+            # 从excel中读取数据
+            self.input_data("./"+self.DATA_SHEET_FILENAME) # './' is necessary when running this file, but should be removed if run main.py
+            print("数据读入完毕，处理中......")
+            # 计算物理量
+            self.calc_data()
+            # 计算不确定度
+            self.calc_uncertainty()
+            print("正在生成实验报告......")
+            # 生成实验报告
+            self.fill_report()
+            print("实验报告生成完毕，正在打开......")
+            os.startfile(self.REPORT_OUTPUT_FILENAME)
+            print("Done!")
     '''
     从excel表格中读取数据
     @param filename: 输入excel的文件名
@@ -92,7 +114,7 @@ class Michelson:
         # 输出带不确定度的最终结果
         bse, pwr = Method.scientific_notation(u_lbd)
         lbd_f = int(lbd * (10 ** pwr)) / (10 ** pwr) # 保留有效数字，截断处理
-        self.data['final'] = "%f±%.0f" % (lbd_f, bse)
+        self.data['final'] = "%.0f±%.0f" % (lbd_f, bse)
     '''
     填充实验报告
     调用ReportWriter类，将数据填入Word文档格式的实验报告中
@@ -113,7 +135,7 @@ class Michelson:
         # 调用ReportWriter类
         RW = ReportWriter()
         RW.load_replace_kw(self.report_data)
-        RW.fill_report("Michelson_empty.docx", "Michelson_out.docx")
+        RW.fill_report(self.REPORT_TEMPLATE_FILENAME, self.REPORT_OUTPUT_FILENAME)
 
 if __name__ == '__main__':
     mc = Michelson()
